5cfeccd1975fe7a8b01a5afa16239a903e6e8be8
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2025-10-03 16:53:34 -0500

    styling manage expense header

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -2,14 +2,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { BottomTabNavigator } from './BottomTabNavigator';
 import { ManageExpenseScreen } from '../screens/ManageExpenseScreen';
+import { GlobalStyles } from '../constants/styles';
 
 const Stack = createNativeStackNavigator();
 
 export const RootStackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        headerTintColor: 'white',
+      }}
+    >
       <Stack.Screen name="Stack" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="ManageExpense" component={ManageExpenseScreen} />
+      <Stack.Screen
+        name="ManageExpense"
+        component={ManageExpenseScreen}
+        options={{
+          presentation: 'modal',
+        }}
+      />
     </Stack.Navigator>
   );
 };
